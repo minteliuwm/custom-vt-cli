a0844f0efde471bf372502eb995a412e964800c5
liuweimin <liuweimin@corp.netease.com>
2020-03-16 10:20:49 +0800

    fix: 脚本命令不生效

--- bin/cli.js
+++ bin/cli.js
@@ -5,16 +5,19 @@ process.env.NODE_PATH = __dirname + '/../node_modules/';
 const { resolve } = require('path');
 const program = require('commander');
 
-const res = command => resolve(__dirname, '../commands/', command);
-
 program.version(require('../package').version );
 
 program.usage('<command>');
 
 program.command('init')
-  .option('-f, --foo', 'enable some foo')
   .description('Generate a new project')
   .alias('i')
   .action(() => {
-    require(res('init'))
+    require('../commands/init');
   });
+
+program.parse(process.argv);
+
+if (!program.args.length) {
+  program.help();
+}
